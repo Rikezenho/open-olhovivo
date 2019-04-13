69bf92abde385983ccc79e02b94fdbb993410b73
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-04-13 01:45:37 -0300

    Changing marker icon

--- src/main.js
+++ src/main.js
@@ -10,9 +10,9 @@ import './registerServiceWorker';
 delete Icon.Default.prototype._getIconUrl;
 
 Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: require('./assets/marker-icon-2x.png'),
+  iconUrl: require('./assets/marker-icon.png'),
+  shadowUrl: require('./assets/marker-shadow.png'),
 });
 
 Vue.config.productionTip = false;
