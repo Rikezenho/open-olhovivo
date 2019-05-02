37862c8ac0d954af49ce7ab972951a1f37a86df9
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-05-01 23:50:50 -0300

    Adding cacheId config in vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,8 @@ const pkg = require('./package.json');
 module.exports = {
   pwa: {
     assetsVersion: pkg.version,
+    workboxOptions: {
+      cacheId: `open-olhovivo-${pkg.version}`,
+    },
   },
 };
