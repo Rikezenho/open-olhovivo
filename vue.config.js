fe19a4c052e60efbd6e24f3678c2683df8336308
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-05-25 17:44:50 -0300

    feat(build): add node_env to package commands
    with node_env, add api url by env too

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
+const webpack = require('webpack');
 const pkg = require('./package.json');
 
+const API_URLS = {
+  development: 'http://localhost:3000',
+  production: 'https://open-olhovivo-api.rikezenho.now.sh',
+};
+
+const API_URL = process.env.NODE_ENV === 'development' ? API_URLS.development : API_URLS.production;
+
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.API_URL': JSON.stringify(API_URL),
+      }),
+    ],
+  },
   pwa: {
     assetsVersion: pkg.version,
     workboxOptions: {
